af18e823097ab73583c7729da061eb4e09a5ffb0
FLzyy <flzyy72@gmail.com>
2023-02-04 09:03:48 +0800

    workarounds for setting cliSpinner when running

--- src/dev.ts
+++ src/dev.ts
@@ -5,5 +5,9 @@ const spinner = new Spinner("Hello World!");
 setTimeout(() => {
   spinner.config = {
     text: "h",
+    cliSpinner: {
+      frames: ["/", "\\"],
+      interval: 1000,
+    },
   };
 }, 2000);
--- src/index.ts
+++ src/index.ts
@@ -23,23 +23,17 @@ export const dots: CliSpinner = {
 
 export default class Spinner {
   text: string;
-  color: string;
-  check: string;
-  x: string;
-  cliSpinner: CliSpinner;
-  stream: NodeJS.WriteStream;
+  color = "33";
+  check = "\x1b[32m✔\x1b[0m";
+  x = "\x1b[31m✘\x1b[0m";
+  stream = process.stdout;
 
+  private s = dots;
   private t: NodeJS.Timer | null;
   private i = 0;
   private l = 0;
 
   constructor(text?: string, config?: SpinnerConfig) {
-    this.color = "33";
-    this.check = "\x1b[32m✔\x1b[0m";
-    this.x = "\x1b[31m✘\x1b[0m";
-    this.cliSpinner = dots;
-    this.stream = process.stdout;
-
     if (config) this.h(config);
     if (text) {
       this.text = text;
@@ -68,9 +62,8 @@ export default class Spinner {
   start(): this {
     if (!this.text) throw new Error("no text");
     if (!this.cliSpinner) throw new Error("no cliSpinner");
-    if (!this.stream.isTTY) return this;
 
-    this.t = setInterval(() => {
+    const self = (): void => {
       this.l = this.text.length;
       this.c().stream.write(
         `\x1b[${this.color}m${this.cliSpinner.frames[this.i++]}\x1b[0m ${
@@ -78,23 +71,29 @@ export default class Spinner {
         }`
       );
       this.i %= this.cliSpinner.frames.length;
-    }, this.cliSpinner.interval ?? 80);
+      this.t = setTimeout(self, this.cliSpinner.interval ?? 80);
+    };
+
+    self();
+
+    return this;
+  }
+
+  private o(): this {
+    if (!this.t) throw new Error("not started");
+    this.i = 0;
+    clearTimeout(this.t);
+    this.t = null;
+    this.l = this.text.length;
 
     return this;
   }
 
   private f(icon: "check" | "x", text?: string, config?: SpinnerConfig): this {
-    if (!this.t) throw new Error("not started");
     if (text) this.text = text;
     if (config) this.h(config);
 
-    this.i = 0;
-    clearInterval(this.t);
-    this.t = null;
-    this.l = this.text.length;
-    this.c().stream.write(
-      `${this[icon]}${text?.padStart(text.length + 1, " ") ?? ""}`
-    );
+    this.o().c().stream.write(`${this[icon]} ${this.text}`);
     return this;
   }
 
@@ -124,13 +123,23 @@ export default class Spinner {
     this.h(cfg);
   }
 
+  set cliSpinner(cspinner: CliSpinner) {
+    this.s = cspinner;
+    if (this.t) this.o().start();
+  }
+
+  get cliSpinner(): CliSpinner {
+    return this.s;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/adjacent-overload-signatures
   get config(): SpinnerConfig {
     return {
       text: this.text,
       color: this.color,
       check: this.check,
       x: this.x,
-      cliSpinner: this.cliSpinner,
+      cliSpinner: this.s,
       stream: this.stream,
     };
   }
